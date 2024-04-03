125f2b77c86ac7cbe25c8e9de1fb382efd74886b
Newton School <newtonschool@Newtons-MacBook-Pro.local>
2024-04-03 13:00:12 +0530

    made changes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import "./App.css";
 import pic from "./assets/NewtonSchool.jpg";
 
 const App = () => {
+  const styles = {
+    width: "130px",
+    height: "100px",
+    margin: "10px",
+  };
   return (
     <div id="navbar">
       <div>NavBar</div>
-      <img />
+      <img src={pic} style={styles}/>
     </div>
   );
 };
