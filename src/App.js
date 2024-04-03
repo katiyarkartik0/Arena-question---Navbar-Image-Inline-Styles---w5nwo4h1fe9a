8b2f2e75317ca059b19ca9db976e5c46b0f24b10
KARTIK KATIYAR <82947445+katiyarkartik0@users.noreply.github.com>
2024-04-03 12:53:47 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import pic from "./assets/NewtonSchool.jpg";
+
+const App = () => {
+  return (
+    <div id="navbar">
+      <div>NavBar</div>
+      <img />
+    </div>
+  );
+};
+
+export default App;
